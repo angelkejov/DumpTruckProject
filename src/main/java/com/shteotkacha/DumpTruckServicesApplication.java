90c96d4304d50765861530579dba718fa2b2d53c
GRIGS <ani_kejov@abv.bg>
2025-07-30 16:12:40 +0300

    railway deployment

--- src/main/java/com/shteotkacha/DumpTruckServicesApplication.java
+++ src/main/java/com/shteotkacha/DumpTruckServicesApplication.java
@@ -3,7 +3,10 @@ package com.shteotkacha;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+    org.springframework.boot.actuate.autoconfigure.metrics.SystemMetricsAutoConfiguration.class,
+    org.springframework.boot.actuate.autoconfigure.metrics.web.tomcat.TomcatMetricsAutoConfiguration.class
+})
 public class DumpTruckServicesApplication {
 
     public static void main(String[] args) {
